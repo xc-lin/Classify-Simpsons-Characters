28afa717a683c24be104d622a59831237aa7cac6
lxc <48900984+xc-lin@users.noreply.github.com>
2021-07-24 15:17:09 +0800

    first commit

--- student.py
+++ student.py
@@ -23,6 +23,7 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torchvision import models
+from torchvision.models import resnet18
 
 """
    Answer to Question:
@@ -56,10 +57,57 @@ def transform(mode):
 class Network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Conv2d(3,64,kernel_size=7,stride=2,padding=3)
-        self.layer2 = nn.MaxPool2d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.layer1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.layer2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer6 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer7 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+        self.layer8 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.layer9 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.layer10 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.layer11 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        self.layer12 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.layer13 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.layer14 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.layer15 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
+        self.layer16 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.layer17 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.layer18 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.layer19 = nn.AdaptiveAvgPool2d((1, 1))
+        self.output = nn.Linear(512, 14)
+
+        self.sub1 = nn.Conv2d(64, 128, kernel_size=1, stride=2, padding=0)
+        self.sub2 = nn.Conv2d(128, 256, kernel_size=1, stride=2, padding=0)
+        self.sub3 = nn.Conv2d(256, 512, kernel_size=1, stride=2, padding=0)
+
     def forward(self, t):
-        print(t.shape)
+        layer1 = self.layer1(t)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)
+        layer4 = self.layer4(layer3)
+        layer5 = self.layer5(layer4 + layer2)
+        layer6 = self.layer6(layer5)
+        layer7 = self.layer7(layer6 + layer4 + layer2)
+        layer8 = self.layer8(layer7)
+        layer_sub1 = self.sub1(layer6 + layer4 + layer2)
+        layer9 = self.layer9(layer8 + layer_sub1)
+        layer10 = self.layer10(layer9)
+        layer11 = self.layer11(layer10 + layer8 + layer_sub1)
+        layer12 = self.layer12(layer11)
+        layer_sub2 = self.sub2(layer10 + layer8 + layer_sub1)
+        layer13 = self.layer13(layer12 + layer_sub2)
+        layer14 = self.layer14(layer13)
+        layer15 = self.layer15(layer14 + layer12 + layer_sub2)
+        layer16 = self.layer16(layer15)
+        layer_sub3 = self.sub3(layer14 + layer12 + layer_sub2)
+        layer17 = self.layer17(layer16 + layer_sub3)
+        layer18 = self.layer18(layer17)
+        layer19 = self.layer19(layer18 + layer16 + layer_sub3)
+        layer19_flattened = torch.flatten(layer19, 1)
+        output = self.output(layer19_flattened)
+        return output
 
 
 class loss(nn.Module):
